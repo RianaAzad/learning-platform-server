485fb0ed5483e7cf0d5d44c11a6bc11964858287
Riana Azad <Riana1996>
2022-10-25 19:50:21 +0600

    cors added

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) =>{
     res.send('api running new');
 });
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
 
 app.listen(port, () => {
     console.log('api running on port', port);
