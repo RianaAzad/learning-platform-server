f8ef5b42187eec78a521720751644a37772d1a37
Riana Azad <Riana1996>
2022-10-27 00:01:50 +0600

    category added

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const courses = require('./data/courses.json');
+const categories = require('./data/categories.json');
 
-app.get('/', (req, res) =>{
-    res.send('api running new');
-});
+const allCourses = require('./data/courses.json');
 
-app.get('/courses', (req, res) => {
-    res.send(courses);
+app.get('/', (req, res)=>{
+    res.send('API RUNNING');
 })
 
-app.get('/course-details/:id', (req, res)=>{
+app.get('/categories', (req, res)=>{
+    res.send(categories);
+})
+
+app.get('/courses/:id', (req, res)=>{
     const id = req.params.id;
-    const selectedCourse = courses.find(course => course.id == id);
-    res.send(selectedCourse);
+    const selectedCourses = allCourses.filter(course => course.category_id == id)
+    res.send(selectedCourses);
+    
 })
 
 app.listen(port, () => {
